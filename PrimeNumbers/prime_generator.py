8d13636d5e9c7a57533dbbd3a7548e883d86f065
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-04-18 13:01:37 +0300

    Final Commit

--- PrimeNumbers/prime_generator.py
+++ PrimeNumbers/prime_generator.py
@@ -1,9 +1,24 @@
 class PrimeGenerator(object):
-
-    n = 0
-
     def __init__(self, n):
         self.n = n
 
-    # def prime_generator(n):
+    def prime_generator(self):
+
+        """ The Function takes O(N^2)"""
+        result = []
+        n = self.n
+        if isinstance(n, int):
+            for x in range(0, n + 1):
+                if x < 2:
+                    r = 0
+                else:
+                    i = 2
+                    while i < x:
+                        if x % i == 0:
+                            break
+                        i = i + 1
+                    else:
+                        result.append(x)
+
+        return result
 
--- PrimeNumbers/test_prime_generator.py
+++ PrimeNumbers/test_prime_generator.py
@@ -5,10 +5,21 @@ import prime_generator
 
 class TestPrimeGenerator(TestCase):
     def setUp(self):
-        self.prime_gen = prime_generator.PrimeGenerator(3)
+        self.prime_gen = prime_generator.PrimeGenerator(1)
+
+    def test_is_integer(self):
+        self.assertIsInstance(self.prime_gen.n, int, msg="Value not an integer")
 
     def test_not_negative(self):
-        self.assertGreater(self.prime_gen.n, 0, msg="Value not positive integer")
+        if isinstance(self.prime_gen.n, int):
+            self.assertGreater(self.prime_gen.n, 0, msg="Value not positive integer")
 
-    def test_is_greater_than_two(self):
-        self.assertGreater(self.prime_gen.n, 2, msg="Value not in the prime number scope")
+    def test_is_greater_than_or_equal_to_two(self):
+        if isinstance(self.prime_gen.n, int):
+            self.assertGreater(self.prime_gen.n, 2, msg="Value not in the prime number scope")
+
+    def test_is_output_a_list_of_integers(self):
+        self.assertTrue(any(isinstance(x, int) for x in self.prime_gen.prime_generator()), msg="Output is not a list of integers")
+
+    def test_is_output_a_list(self):
+        self.assertTrue(isinstance(self.prime_gen.prime_generator(), list), msg="Output is not a list")
